5d30278f59ee754b5373c62be8439babad58a930
Ben Bond-Lamberty <bondlamberty@pnnl.gov>
2016-02-02 12:59:34 -0500

    Script updates after working with Peyton and Taniya

--- scripts/picarro.R
+++ scripts/picarro.R
@@ -14,14 +14,24 @@ filelist <- list.files("picarrodata/", pattern = "*.gz", full.names = TRUE)
 rawdata <- list()
 for(f in filelist) {
   cat("Reading", f, "\n")
-  rawdata[[f]] <- read_table(f) 
+  read_table(f) %>%
+    select(DATE, TIME, ALARM_STATUS, MPVPosition, CH4_dry, CO2_dry) ->
+    rawdata[[f]]
 }
 rawdata <- rbind_all(rawdata)
+
+# Make sure that no instrument alarm was triggered
+if(any(rawdata$ALARM_STATUS != 0)) {
+  warning("There are non-zero alarm statuses in this dataset!")  
+}
 
 # Change the `DATE` and `TIME` character fields into a single date field
 # Be careful: know date and time on instrument versus local time
 print("Computing DATETIME...")
-rawdata$DATETIME <- ymd_hms(paste(rawdata$DATE, rawdata$TIME))
+rawdata$DATETIME <- lubridate::ymd_hms(paste(rawdata$DATE, rawdata$TIME))
+
+# Get rid of unneeded fields
+rawdata$DATE <- rawdata$TIME <- NULL
 
 # Assign sample numbers
 # We've seen this code before!
@@ -32,7 +42,7 @@ rawdata$samplenum <- c(TRUE, vnums[-length(vnums)] != vnums[-1]) %>%
 print(summary(rawdata$samplenum))
 
 # Make a plot. What's the problem?
-uglyplot <- qplot(DATETIME, CO2_dry, data=rawdata, color=factor(MPVPosition))
+uglyplot <- qplot(DATETIME, CO2_dry, data = rawdata, color = factor(MPVPosition))
 uglyplot <- uglyplot + scale_color_discrete("Valve")
 print(uglyplot)
 
@@ -41,13 +51,27 @@ valvedata <- read_csv("picarrodata/valvedata.csv")
 
 # TODO: a dplyr pipeline that
 # (1) filters out fractional valve values
-# (2) removes all but a few fields (samplenum, DATETIME, MPVPosition, CH4_dry, CO2_dry)
-# (3) merges the data with `valvedata`
+# (2) merges the data with `valvedata`
+# (3) arranges the data by DATETIME
 # (4) groups the data by samplenum
-# (5) arranges the data by DATETIME
-# (6) computes elapsed_seconds for each sample (using `mutate`)
-# (7) filters out soil core "J"
+# (5) computes elapsed_seconds for each sample (using `mutate`)
+# (8) filters out soil core "J"
 # rawdata %>% ...  -> cleandata
+
+# Solution
+rawdata %>%
+  filter(MPVPosition == floor(MPVPosition)) %>%
+  left_join(valvedata, by = "MPVPosition") %>%
+  arrange(DATETIME) %>%
+  group_by(samplenum) %>%
+  mutate(elapsed_seconds = as.numeric(DATETIME - min(DATETIME))) %>%
+  filter(elapsed_seconds >= 2.0) %>%   # first few seconds are "wonky" sometimes
+  filter(soilcore != "J") ->   # J is ambient
+  cleandata
+
+prettyplot <- qplot(elapsed_seconds, CO2_dry, data = cleandata, color = soilcore, group = samplenum, geom = "line")
+print(prettyplot)
+
 
 # TODO: make a pretty plot: elapsed_seconds (x) by CO2_dry (y), colored by soilcore
 
